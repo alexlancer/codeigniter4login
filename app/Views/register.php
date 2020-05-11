777c4a7d6b9177c29480f691a3607a48efac659f
alexlancer <63387846+alexlancer@users.noreply.github.com>
2020-05-11 18:48:52 +0300

    Update register.php

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,6 +1,6 @@
 <div class="container">
   <div class="row">
-    <div class="col-12 col-sm8- offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white from-wrapper">
+    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white from-wrapper">
       <div class="container">
         <h3>Register</h3>
         <hr>
