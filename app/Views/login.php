32e273263e3da0269116242cc05e7e403af597d5
MohamedDrine <mohamed.drine@outlook.fr>
2021-03-08 17:54:00 +0100

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -30,7 +30,7 @@
               <button type="submit" class="btn btn-primary">Login</button>
             </div>
             <div class="col-12 col-sm-8 text-right">
-              <a href="/register">Don't have an account yet?</a>
+              <a href="/register">Dzaeaon't have an account yet?</a>
             </div>
           </div>
         </form>
